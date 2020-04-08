a460c71c5445dfa44825c9a67ab53b0028dd6880
Basel <basel.assafff@gmail.com>
2020-04-09 02:21:46 +0300

    angular-jwt

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
-
+import {JwtHelperService} from '@auth0/angular-jwt';
+ 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
 baseUrl = 'http://localhost:5000/api/auth/';
+jwtHelper =  new JwtHelperService();
+decodedToken: any;
 
 constructor(private http: HttpClient) { }
 
@@ -15,8 +18,10 @@ login(model: any) {
 return this.http.post(this.baseUrl + 'login', model).pipe(
   map((response: any) => {
     const user = response;
-    if(user) {
+    if (user) {
       localStorage.setItem('token', user.token);
+      this.decodedToken = this.jwtHelper.decodeToken(user.token);
+      console.log(this.decodedToken);
     }
   })
 );
@@ -24,5 +29,9 @@ return this.http.post(this.baseUrl + 'login', model).pipe(
 
 register(model: any) {
   return this.http.post(this.baseUrl + 'register', model);
+}
+loggedIn() {
+  const token = localStorage.getItem('token');
+  return !this.jwtHelper.isTokenExpired(token);
 }
 }
--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -10,7 +10,7 @@ import { AlertifyService } from '../_services/alertify.service';
 export class NavComponent implements OnInit {
 //  create obj called model of type "any" for un and pw
   model: any = {};
-  constructor(private authService: AuthService, private alertify: AlertifyService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
@@ -24,9 +24,7 @@ export class NavComponent implements OnInit {
   }
 
   loggedIn() {
-   const token = localStorage.getItem('token');
-  //  check if token have value return t or f
-   return !!token;
+    return this.authService.loggedIn();
   }
 
   logout() {
