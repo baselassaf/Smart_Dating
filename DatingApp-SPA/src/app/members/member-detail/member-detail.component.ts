aa7e3ca07f0b61d83ed4494f030e08f1d1ef6af6
Basel <basel.assafff@gmail.com>
2020-07-18 18:00:16 +0300

    messages SPA

--- DatingApp-SPA/src/app/members/member-detail/member-detail.component.ts
+++ DatingApp-SPA/src/app/members/member-detail/member-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { User } from '../../_models/user';
 import { UserService } from '../../_services/user.service';
 import { AlertifyService } from 'src/app/_services/alertify.service';
@@ -8,6 +8,8 @@ import {
   NgxGalleryImage,
   NgxGalleryAnimation,
 } from '@kolkov/ngx-gallery';
+import { TabsetComponent } from 'ngx-bootstrap/tabs';
+
 
 @Component({
   selector: 'app-member-detail',
@@ -15,6 +17,7 @@ import {
   styleUrls: ['./member-detail.component.css'],
 })
 export class MemberDetailComponent implements OnInit {
+  @ViewChild('memberTabs', {static: true}) memberTabs: TabsetComponent;
   user: User;
   galleryOptions: NgxGalleryOptions[];
   galleryImages: NgxGalleryImage[];
@@ -29,6 +32,11 @@ export class MemberDetailComponent implements OnInit {
     this.route.data.subscribe((data) => {
       this.user = data['user'];
     });
+
+    this.route.queryParams.subscribe(params => {
+      const selectedTab = params['tab'];
+      this.memberTabs.tabs[selectedTab > 0 ? selectedTab : 0].active = true;
+    })
 
     this.galleryOptions = [
       {
@@ -54,5 +62,9 @@ export class MemberDetailComponent implements OnInit {
       });
     }
     return imageUrls;
+  }
+
+  selectTab(tabId: number) {
+    this.memberTabs.tabs[tabId].active = true;
   }
 }
--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -11,6 +11,7 @@ import { MemberEditComponent } from './members/member-edit/member-edit.component
 import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes-guard';
 import { ListstResolver } from './_resolvers/lists.resolver';
+import { MessagesResolver } from './_resolvers/messages-resolver';
 
 export const appRoute: Routes = [
     {path: '', component: HomeComponent},
@@ -25,7 +26,7 @@ export const appRoute: Routes = [
              resolve: {user: MemberDetailResolver}} ,
             {path: 'member/edit', component: MemberEditComponent,
             resolve: {user: MemberEditResolver}, canDeactivate: [PreventUnsavedChanges]},
-            {path: 'messages', component: MessagesComponent},
+            {path: 'messages', component: MessagesComponent, resolve: {messages: MessagesResolver}},
             {path: 'lists', component: ListsComponent, resolve: {users: ListstResolver}},
         ]
     },
